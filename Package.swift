4d0be14997d28bb0e5f565f9df584786fc47b19a
lovexiaoxia <aadyx2007@163.com>
2025-11-05 17:13:57 +0800

    Fix

--- Package.swift
+++ Package.swift
@@ -3,11 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "MKSwiftUILibrary",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v15)], // 保持 iOS 15
     products: [
         .library(
             name: "MKSwiftUILibrary",
-            type: .dynamic,
             targets: ["MKSwiftUILibrary"]
         ),
     ],
@@ -25,12 +24,6 @@ let package = Package(
             swiftSettings: [
                 .define("DEBUG", .when(configuration: .debug)),
                 .define("IOS15_OR_LATER"),
-            ],
-            linkerSettings: [
-                .linkedLibrary("z"),
-                .linkedLibrary("iconv"),
-                .linkedFramework("Foundation"),
-                .linkedFramework("UIKit")
             ]
         ),
         .testTarget(
